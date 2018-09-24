b2c8777867eddb73ddeae4c5cf1ad8e00972eeae
mishina <temma182008@gmail.com>
2018-09-24 12:49:09 +0900

    RecordStatisticsControllerTest 追加

--- test/controllers/record_statistics_controller_test.rb
+++ test/controllers/record_statistics_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RecordStatisticsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get record_statistics_url
+    assert_response :success
+  end
 end
