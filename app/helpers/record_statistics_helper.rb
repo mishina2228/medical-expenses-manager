d9ee12c97951457a3bb7dda185591e4ab68e8f02
mishina <temma182008@gmail.com>
2018-12-23 21:41:58 +0900

    統計画面をレスポンシブにしたい

--- app/helpers/record_statistics_helper.rb
+++ app/helpers/record_statistics_helper.rb
@@ -2,4 +2,15 @@ module RecordStatisticsHelper
   def check_division(division, klass)
     division.class == klass ? 'glyphicon-check' : 'glyphicon-unchecked'
   end
+
+  def render_division(division)
+    case division.class.to_s
+    when Drugstore.to_s
+      t('text.division_types.drug')
+    when Transport.to_s
+      t('text.division_types.other')
+    when Hospital.to_s
+      t('text.division_types.medical_treatment')
+    end
+  end
 end
