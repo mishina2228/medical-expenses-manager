03c1d191fac7db04c1f6a09d5058973ad7c3b4d3
mishina <temma182008@gmail.com>
2019-02-24 17:19:57 +0900

    collection_select を使用するように修正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.objects_for_select
-    all.map {|o| [o.name, o.id]}
-  end
-
   def self.paginate(params)
     per = params[:per] || 30
     page(params[:page]).per(per)
