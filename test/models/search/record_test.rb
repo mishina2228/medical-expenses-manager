d2f134f39648ba39a3725721fe566815793be1da
mishina <temma182008@gmail.com>
2019-12-07 11:18:03 +0900

    rewrite test with assert style

--- test/models/search/record_test.rb
+++ test/models/search/record_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Search::RecordTest < ActiveSupport::TestCase
-  def test_days_of_month
+  test 'days_of_month' do
     record = Search::Record.new(month: '2018-02')
     expected = Time.new(2018, 2, 1)..Time.new(2018, 2, 28).end_of_day
     assert_equal expected, record.days_of_month
