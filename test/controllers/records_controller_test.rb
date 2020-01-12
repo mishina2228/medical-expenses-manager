48998d7776a01e57f5b874c0f8535fbc27b9ae96
mishina <temma182008@gmail.com>
2020-01-12 21:36:12 +0900

    add controller test

--- test/controllers/records_controller_test.rb
+++ test/controllers/records_controller_test.rb
@@ -31,7 +31,47 @@ class RecordsControllerTest < ActionDispatch::IntegrationTest
            }
     end
 
+    assert_response :redirect
     assert_redirected_to records_url
+  end
+
+  test 'should create records continuously when continuous parameter is given' do
+    Record.delete_all
+    assert_equal 2, @record.hospital.hospital_transports.size
+    record_params = {
+      record: {
+        cost: @record.cost,
+        date: @record.date,
+        person_id: @record.person_id,
+        division_id: @record.division_id
+      }
+    }
+
+    assert_difference -> {Record.count}, 3 do
+      post records_url, params: record_params.merge(continuous: true)
+    end
+
+    assert_response :redirect
+    assert_redirected_to new_record_url(record_params)
+  end
+
+  test 'should not create record unless record is valid' do
+    @record.date = nil
+    assert @record.invalid?
+
+    assert_no_difference -> {Record.count} do
+      post records_url,
+           params: {
+             record: {
+               cost: @record.cost,
+               date: @record.date,
+               person_id: @record.person_id,
+               division_id: @record.division_id
+             }
+           }
+    end
+
+    assert_response :success
   end
 
   test 'should show record' do
@@ -51,7 +91,23 @@ class RecordsControllerTest < ActionDispatch::IntegrationTest
               cost: @record.cost
             }
           }
+
+    assert_response :redirect
     assert_redirected_to records_url
+  end
+
+  test 'should not update record unless record is valid' do
+    @record.cost = nil
+    assert @record.invalid?
+
+    patch record_url(id: @record),
+          params: {
+            record: {
+              cost: @record.cost
+            }
+          }
+
+    assert_response :success
   end
 
   test 'should destroy record' do
@@ -59,6 +115,7 @@ class RecordsControllerTest < ActionDispatch::IntegrationTest
       delete record_url(id: @record)
     end
 
+    assert_response :redirect
     assert_redirected_to records_url
   end
 end
