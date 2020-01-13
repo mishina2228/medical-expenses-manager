4311a8f844516247dbe791066219b6368221c1c7
mishina <temma182008@gmail.com>
2020-01-13 11:13:39 +0900

    [WIP] improve test coverage

---
+++ test/controllers/divisions_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class DivisionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @drugstore = drugstores(:drugstore1)
+  end
+
+  test 'should get all division names' do
+    
+  end
+end
