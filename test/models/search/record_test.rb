0c256dd60f0a9dbd01ccb793adc7731ecefcd4db
mishina <temma182008@gmail.com>
2021-01-21 23:45:14 +0900

    My wrong: Fix the test not to fail due to timezone differences, again.

--- test/models/search/record_test.rb
+++ test/models/search/record_test.rb
@@ -103,7 +103,7 @@ class Search::RecordTest < ActiveSupport::TestCase
 
   test 'days_of_month' do
     record = Search::Record.new(month: '2018-02')
-    expected = Time.zone.local(2018, 2, 1)..Time.zone.local(2018, 2, 28).end_of_day
+    expected = Date.new(2018, 2, 1).beginning_of_day..Date.new(2018, 2, 28).end_of_day
     assert_equal expected, record.days_of_month
   end
 end
