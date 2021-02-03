37438d495b88a11202e541d00f066b8d7b6430d3
mishina <temma182008@gmail.com>
2021-02-04 07:48:23 +0900

    Disable Asset Pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,11 +24,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
