b31e1d5ea6d23dee48bb11ab6ca86e2b5c57da9d
mishina <temma182008@gmail.com>
2021-02-10 23:28:39 +0900

    Introduce webpack-bundle-analyzer

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
+environment.plugins.append(
+  'BundleAnalyzer',
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false
+  })
+)
 module.exports = environment.toWebpackConfig()
