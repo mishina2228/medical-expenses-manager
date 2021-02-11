379cbedfa57330f6048e0f3b3a2705a6428dd97e
mishina <temma182008@gmail.com>
2021-02-11 14:37:55 +0900

    Add fa-chevron-left and fa-chevron-right required by tempusdominus-bootstrap-4

--- app/packs/src/fontawesome.js
+++ app/packs/src/fontawesome.js
@@ -1,9 +1,9 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
-import { faCalendar, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCalendar, faChevronLeft, faChevronRight, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faCheckSquare, faComments, faSquare } from '@fortawesome/free-regular-svg-icons'
 
 library.add(
-  faCalendar, faMinus, faPlus,
+  faCalendar, faChevronLeft, faChevronRight, faMinus, faPlus,
   faCheckSquare, faComments, faSquare
 )
 
