23c6eea25f9f53940fdaeabeb63be6bb39a6df96
mishina <temma182008@gmail.com>
2021-02-18 23:03:04 +0900

    Refactoring

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -16,15 +16,11 @@ import '../src/theme_changer'
 
 import './application.scss'
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-require.context('../images', true)
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
 
-require('@rails/ujs').start()
-require('turbolinks').start()
-require('@rails/activestorage').start()
-require('jquery')
+Rails.start()
+Turbolinks.start()
 require('@nathanvda/cocoon')
-require('js-cookie')
-require('emoji-toolkit')
+
+require.context('../images', true)
