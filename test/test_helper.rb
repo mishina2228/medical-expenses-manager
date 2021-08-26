6dedca06d1626ee6860227d1278589c8a63048c3
mishina <temma182008@gmail.com>
2021-08-26 19:58:57 +0900

    written in a different place, wao.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,19 +10,6 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'minitest/autorun'
 
-Capybara.configure do |config|
-  config.default_driver = :selenium_chrome_headless
-end
-
-Capybara.register_driver :selenium_chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--disable-dev-shm-usage')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
