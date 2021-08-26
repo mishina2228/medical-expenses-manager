3e78de4b8333e6a5058038d3774131eaec5bbf38
mishina <temma182008@gmail.com>
2021-08-26 19:04:49 +0900

    add some arguments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,8 @@ require 'minitest/autorun'
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
