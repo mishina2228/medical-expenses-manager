e9ac2c5fa718998c45568c185c3a9661acfa4c85
mishina <temma182008@gmail.com>
2021-11-14 12:00:36 +0900

    refactor: Rewrite in Vanilla JS

--- app/packs/src/record.js
+++ app/packs/src/record.js
@@ -3,15 +3,19 @@ import emojiToolkit from 'emoji-toolkit'
 window.addEventListener('turbolinks:load', () => {
   emojify()
 
-  $('.division_type').on('change', event => {
-    loadDivisionIds($(event.currentTarget).val())
+  const divisionTypeSelector = document.querySelector('.division_type')
+  divisionTypeSelector?.addEventListener('change', event => {
+    loadDivisionIds(event.currentTarget.value)
   })
 
-  $('#reset_search').on('click', resetSearchForm)
+  const resetBtn = document.getElementById('reset_search')
+  resetBtn?.addEventListener('click', resetSearchForm)
 
-  $('#csv_loader').on('change', event => {
-    propLoadButton($(event.currentTarget))
-  }).change()
+  const csvLoaderBtn = document.getElementById('csv_loader')
+  csvLoaderBtn?.addEventListener('change', event => {
+    propLoadButton(event.currentTarget)
+  })
+  csvLoaderBtn?.dispatchEvent(new window.Event('change'))
 })
 
 const loadDivisionIds = klassName => {
@@ -41,12 +45,13 @@ const resetSearchForm = () => {
   fields.forEach(elem => { elem.value = '' })
 }
 
-const propLoadButton = $csvLoader => {
-  const $csvLoadButton = $('#csv_load_button')
-  if ($csvLoader[0].files[0] && extname($csvLoader[0].files[0].name) === 'csv') {
-    $csvLoadButton.prop('disabled', false)
+const propLoadButton = csvLoader => {
+  const csvLoadButton = document.getElementById('csv_load_button')
+
+  if (csvLoader.files[0] && extname(csvLoader.files[0].name) === 'csv') {
+    csvLoadButton.removeAttribute('disabled')
   } else {
-    $csvLoadButton.prop('disabled', true)
+    csvLoadButton.setAttribute('disabled', 'disabled')
   }
 }
 
