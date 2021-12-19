8c02a3cc69b6c2d00f90a2a9ee04ff7dc02f8cee
mishina <temma182008@gmail.com>
2021-12-19 18:01:24 +0900

    style: cosme

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module MedicalManager
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
   end
