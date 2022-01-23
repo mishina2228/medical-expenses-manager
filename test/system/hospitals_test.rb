a051c132a57bd3b24146aec3741511ab35f7cc7c
mishina <temma182008@gmail.com>
2022-01-23 23:05:47 +0900

    test: Add system tests for hospital transports
    Sanity check for cocoon

--- test/system/hospitals_test.rb
+++ test/system/hospitals_test.rb
@@ -24,6 +24,22 @@ class HospitalsTest < ApplicationSystemTestCase
     page.assert_current_path(hospitals_path)
   end
 
+  test 'create a hospital with transports' do
+    visit hospitals_url
+    click_on I18n.t('helpers.link.new')
+
+    fill_in Hospital.human_attribute_name(:name), with: @hospital.name + Time.current.usec.to_s
+    find('a.add_fields').click
+    find("select[name$='[transport_id]']").select(transports(:transport1).name)
+    find("input[name$='[transport_cost]']").set(42)
+    click_on I18n.t('helpers.submit.create')
+
+    assert_text I18n.t('helpers.notice.create')
+    text = I18n.t('helpers.title.list', models: Hospital.model_name.human.pluralize(I18n.locale))
+    assert_selector 'h1', text: text
+    page.assert_current_path(hospitals_path)
+  end
+
   test 'create a hospital with invalid parameters' do
     visit hospitals_url
     click_on I18n.t('helpers.link.new')
@@ -48,6 +64,21 @@ class HospitalsTest < ApplicationSystemTestCase
     assert_text I18n.t('helpers.notice.update')
     page.assert_current_path(hospitals_path)
     assert_equal after_name, @hospital.reload.name
+  end
+
+  test 'update a hospital with transports' do
+    visit hospitals_url
+    click_on @hospital.name, match: :first
+    click_on I18n.t('helpers.link.edit')
+    page.assert_current_path(edit_hospital_path(id: @hospital.id))
+
+    all('a.add_fields').last.click
+    all("select[name$='[transport_id]']").last.select(transports(:plane).name)
+    all("input[name$='[transport_cost]']").last.set(42)
+    click_on I18n.t('helpers.submit.update')
+
+    assert_text I18n.t('helpers.notice.update')
+    page.assert_current_path(hospitals_path)
   end
 
   test 'update a hospital with invalid parameters' do
