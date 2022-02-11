4cad73c863b421f4003ac93904ff19fafab86b1b
mishina <temma182008@gmail.com>
2022-02-11 11:22:41 +0900

    chore: Set the current schema version by `rails app:update`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_030428) do
+ActiveRecord::Schema[6.1].define(version: 2021_01_09_030428) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
