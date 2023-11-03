ab64383402dfd06033373c3ac8cab650134f0c4f
mishina <temma182008@gmail.com>
2023-11-03 16:52:32 +0900

    chore: Set the `config.load_defaults` to `7.1`

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 module MedicalManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -33,6 +33,6 @@ module MedicalManager
     config.active_record.default_timezone = :local
 
     # Change the format of the cache entry.
-    # config.active_support.cache_format_version = 7.1
+    config.active_support.cache_format_version = 7.1
   end
 end
